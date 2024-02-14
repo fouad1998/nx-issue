f23761591af294235489286519a1150500090bcb
Hachour Fouad <hachour5fouad@gmail.com>
2024-02-14 10:51:34 +0100

    wip

--- apps/access-manager/src/app/app.tsx
+++ apps/access-manager/src/app/app.tsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
 
+import Calexa from 'calexa/Module';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
       <NxWelcome title="access-manager" />
+      <Calexa />
     </div>
   );
 }
---
+++ apps/calexa/src/app/app.tsx
@@ -0,0 +1,13 @@
+import Test from 'test/test';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="calexa" />
+      <Test />
+    </div>
+  );
+}
+
+export default App;
